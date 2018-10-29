39e083bb957de302da7131880e242a653f0060ef
1v1expert <1v1expert@gmail.com>
2018-10-29 18:06:34 +0300

    Fix

--- AI_speak.py
+++ AI_speak.py
@@ -2,11 +2,6 @@ from telebot import *
 from config import *
 import grab
 
-bot = TeleBot(TELEGRAM_TOKEN)
-
-g = grab.Grab()
-g.go('http://aiproject.ru/')
-
 @bot.message_handler(commands=['start'])
 def init(message):
     bot.send_message(message.chat.id, work_with_alice("Привет"))
@@ -21,5 +16,8 @@ def work_with_alice(answer):
     return g.xpath_text('//*[@id="top"]/div/div[2]/p[3]')[6:]
 
 if __name__=='__main__':
+    bot = TeleBot(TELEGRAM_TOKEN)
+    g = grab.Grab()
+    g.go('http://aiproject.ru/')
     bot.polling(none_stop=True)
     
